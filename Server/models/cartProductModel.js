61ce3536517bed2232894b07342f1e86d0bd48aa
dipesh <dipeshydv334@gmail.com>
2025-11-02 22:05:11 +0530

    cart product mdoel update code

--- Server/models/cartProductModel.js
+++ Server/models/cartProductModel.js
@@ -4,11 +4,7 @@ import mongoose from 'mongoose' ;
 const cartProductSchema = new mongoose.Schema({
     productId: {
         type: mongoose.Schema.ObjectId,
-<<<<<<< HEAD
         ref: 'product'
-=======
-        ref: 'Product'
->>>>>>> d8f2562b69d0e4cd6621ad29612b6617aeb9b60d
     },
     quantity: {
         type: Number,
