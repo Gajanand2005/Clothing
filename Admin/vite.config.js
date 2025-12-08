0d239ba8731e739ea9eb1bbd4a29e9ef9900fe64
gajanand <gajanand2005singh@gmail.com>
2025-12-08 12:04:16 +0530

    done

--- Admin/vite.config.js
+++ Admin/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase limit to 1MB
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';  // Split node_modules into vendor chunk
+          }
+        },
+      },
+    },
+  },
 })
